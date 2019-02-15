e6f29bd58c76841a0366afad1f892580db52bae9
dpallero25 <dpallero@dfuls.cl>
2019-02-14 22:00:32 -0300

    a ver si sale

---
+++ try2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+
+
+
+
+
+
+# $log_{10}$M$_{\odot}$ = 14.3$^{-1}$
+
+# In[2]:
+
+
+print 'funciona basura'
+
+
+# In[3]:
+
+
+a = [1,2,3,4,5,6]
+b = a[4:6]
+
+
+# In[9]:
+
+
+c = a*b
+
+
+# In[6]:
+
+
+d = np.asarray([b])**2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
